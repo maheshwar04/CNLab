e28fff3cac7ed9596f7249153abf35e8ef12a786
Maheshwar04 <116495825+maheshwar04@users.noreply.github.com>
2023-10-17 22:04:30 +0530

    updated

--- Maheshwar/CN/Ex2o1.java
+++ Maheshwar/CN/Ex2o1.java
@@ -2,26 +2,20 @@ import java.util.*;
 import java.io.*;
 public class Ex2o1 {
   public static void main(String[] args) throws IOException {
-   // FileOutputStream f=new FileOutputStream("file.txt");
-    BufferedWriter writer = new BufferedWriter(new FileWriter("file.txt"));
+   FileOutputStream f=new FileOutputStream("text.txt");
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the text and put @ to end");
-    //char ch;
-    String str;
+    char ch;
     while(true){
-    str=sc.nextLine();
-    writer.write(str);
-    writer.close();
-    // ch=(char)sc.next().charAt(0);
-    // if(ch!='@'){
-    //     f.write(ch);
-    // }else{
-    //     break;
-    // }
-    //f.close();
-    //}
+    ch=(char)sc.next().charAt(0);
+    if(ch!='@'){
+        f.write(ch);
+    }else{
+        break;
+    }
+    f.close();
+    }
     System.out.println("the file is sucessfully saved");
     
   }
-}
 }
