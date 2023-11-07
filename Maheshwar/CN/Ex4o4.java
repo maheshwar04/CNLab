edfcba4981a66e19814cab357edc679f7efb07c2
Maheshwar04 <116495825+maheshwar04@users.noreply.github.com>
2023-11-08 00:03:33 +0530

    updated

--- Maheshwar/CN/Ex4o4.java
+++ Maheshwar/CN/Ex4o4.java
@@ -1,6 +1,11 @@
-public class Ex4o4 {
-    public static void main(String[] args) {
-        System.out.println();
-
-    }
+    import java.net.*;
+    import java.util.*;
+    public class Ex4o4 {
+     public static void main(String[] args) throws SocketException {
+     Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+     while (interfaces.hasMoreElements()) {
+     NetworkInterface ni = interfaces.nextElement();
+     System.out.println(ni);
+     }
+}
 }
